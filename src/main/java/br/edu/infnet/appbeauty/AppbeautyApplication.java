067437fe2a0fded72bb1f5c485258a8f3c8cdf70
Sandro Andre Janotte <sjanotte@gmail.com>
2022-08-04 00:20:12 -0300

    Criado o projeto appbeauty

---
+++ src/main/java/br/edu/infnet/appbeauty/AppbeautyApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/appbeauty/AppbeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appbeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
